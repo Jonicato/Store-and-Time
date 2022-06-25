851e91b9e2a1f693dc1123bd44d8e12bcdd4111d
Jonathan Rodriguez <jhonarodriguez98@gmail.com>
2022-06-25 17:02:36 -0500

    Clase de GET para recibir parámetros añadida

--- index.js
+++ index.js
@@ -6,51 +6,47 @@ app.get('/', (req, res) => {
   res.send('Hola, este es mi primer server en express');
 });
 
-app.get('/nuevaRuta', (req, res) => {
+app.get('/newPath', (req, res) => {
   res.send('Hola, soy un nuevo endpoint');
 });
 
-app.get('/empresa', (req, res) => {
-  res.json({
-    empresa: 'Morpheus DSS',
-    ceo: 'David Sámano Ferreira',
-    lider: 'Adriana Molina López',
-    departamentos: [
-      {
-        area: 'Aplicaciones móviles',
-        encargado: 'Ritchie'
-      },
-      {
-        area: 'Desarrollo Web y Escritorio',
-        encargado: 'Chuche'
-      },
-      {
-        area: 'APIS',
-        encargado: 'Mauricio'
-      },
-      {
-        area: 'Administración de Bases de Datos',
-        encargado: 'Paty'
-      },
-      {
-        area: 'DevOps',
-        encargado: 'Pizano'
-      },
-      {
-        area: 'Soporte',
-        encargado: 'Luly'
-      },
-      {
-        area: 'Calidad',
-        encargado: 'Jesu'
-      },
-      {
-        area: 'Prototipos',
-        encargado: 'Zuz'
-      }
-    ]
-  });
+app.get('/products', (req, res) => {
+  res.json(
+    {
+      name: 'Reloj 1',
+      price: '1550'
+    },
+    {
+      name: 'Reloj 2',
+      price: '1650'
+    },
+    {
+      name: 'Reloj 3',
+      price: '1750'
+    },
+    {
+      name: 'Reloj 4',
+      price: '1850'
+    }
+  );
 });
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+      id,
+      name: 'Reloj 4',
+      price: '1850'
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoriaId, productoId} = req.params;
+  res.json({
+    categoriaId,
+    productoId
+  })
+})
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
