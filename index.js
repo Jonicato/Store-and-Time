4d9ad98430f13140c35d445bd476b7695f8a93d6
Jonicato <jhonarodriguez98@gmail.com>
2022-06-28 15:25:49 -0500

    Clase de POST añadida

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+/* Es un middleware de express que nos permite trabajar con JSON */
+app.use(express.json());
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 });
