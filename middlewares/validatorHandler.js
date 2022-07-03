997a09d9e64af1fac0496b4ab11ec83760d62fc2
Jonathan Rodriguez <jhonarodriguez98@gmail.com>
2022-07-03 09:37:17 -0500

    Primeras validaciones con esquema añadidas a las rutas

--- middlewares/validatorHandler.js
+++ middlewares/validatorHandler.js
@@ -1,9 +1,10 @@
 const boom = require('@hapi/boom');
 
+//Estamos creando un middleware dinámico, ya que le pasamos el esquema y la propiedad para que encuentre los valores por donde sean enviados.
 function validatorHandler(schema, property){
   return (req, res, next) => {
     const data = req[property];
-    const {error} = schema.validate(data);
+    const {error} = schema.validate(data, {abortEarly: false});
     if(error) {
       next(boom.badRequest(error));
     }
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ProductsService = require('./../services/product');
-
-const productsService = require('./../services/product')
+const validatorHandler = require('./../middlewares/validatorHandler');
+const {createProductSchema, updateProductSchema, getProductSchema} = require('./../schemas/productSchema');
 
 const router = express.Router();
 const service = new ProductsService();
@@ -15,7 +15,9 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
   try {
     const {id} = req.params;
     const product = await service.findOne(id);
@@ -25,15 +27,20 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res, next) => {
+router.patch('/:id',
+  validatorHandler(updateProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
   try {
-  const {id} = req.params;
+    const {id} = req.params;
     const body = req.body;
     const product = await service.update(id, body);
     res.json(product);
