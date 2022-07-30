8f0de912f6ef2eafc3f21a98211a390ca6dc1111
Jonicato <jhonarodriguez98@gmail.com>
2022-07-30 09:57:50 -0500

    Corriendo migraciones a postgres y corrección de errores de conexión

---
+++ db/migrations/20220730143044-create-user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const {UserSchema, USER_TABLE} = require('./../models/userModel')
+
+module.exports = {
+  //Aquí se va a hacer la creación
+  async up (queryInterface) {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+  },
+
+  //Aquí puedo revertir en caso de que haya un error
+  async down (queryInterface, Sequelize) {
+    await queryInterface.drop(USER_TABLE);
+  }
+};
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -14,6 +14,6 @@ const sequelize = new Sequelize(URI, {
 
 setupModels(sequelize);
 
-sequelize.sync(); //Esto es muy delicado y no se aconseja hacer en producción
+//sequelize.sync(); Esto es muy delicado y no se aconseja hacer en producción
 
 module.exports = sequelize;
