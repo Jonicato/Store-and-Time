65e74bbbf6dc40b2f8944a9f862bad1a0d491bba
Jonicato <jhonarodriguez98@gmail.com>
2022-11-22 14:56:37 -0600

    Se agregó la tabla customers y sus respectivos componentes

---
+++ db/migrations/20220827164229-create-customers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { CustomerSchema, CUSTOMER_TABLE} = require('./../models/customerModel')
+
+module.exports = {
+  //Aquí se va a hacer la creación
+  async up (queryInterface) {
+    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+  },
+
+  //Aquí puedo revertir en caso de que haya un error
+  async down (queryInterface) {
+    await queryInterface.drop(CUSTOMER_TABLE);
+  }
+};
--- db/models/index.js
+++ db/models/index.js
@@ -1,11 +1,15 @@
-const { Product, ProductSchema } = require('./productModel');
 const { User, UserSchema } = require('./userModel');
+const { Product, ProductSchema } = require('./productModel');
 const { Category, CategorySchema } = require('./categoryModel');
+const { Customer, CustomerSchema } = require('./customerModel');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
+
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
